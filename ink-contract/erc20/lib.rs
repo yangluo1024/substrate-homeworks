89683f29b0ab6b720adb1496d84fc4da38a05695
yang <yangluo1024@163.com>
2022-01-22 19:35:16 +0800

    220122

---
+++ ink-contract/erc20/lib.rs
@@ -0,0 +1,141 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod erc20 {
+    use ink_storage::{
+        lazy::Lazy,
+        collections::HashMap as StorageHashMap,
+    };
+    use scale::{Encode, Decode};
+
+    #[ink(storage)]
+    pub struct Erc20 {
+        total_supply: Lazy<Balance>,
+        balances: StorageHashMap<AccountId, Balance>,
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>,
+    }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        #[ink(topic)]
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        #[ink(topic)]
+        value: Balance,
+    }
+
+    #[derive(Debug, PartialEq, Eq, Encode, Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
+        InsufficientAllowance,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    impl Erc20 {
+        #[ink(constructor)]
+        pub fn new(supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = StorageHashMap::new();
+            balances.insert(caller, supply);
+
+            Self::env().emit_event( Transfer {
+                from: None,
+                to: Some(caller),
+                value: supply,
+            });
+
+            Self {
+                total_supply: Lazy::new(supply),
+                balances,
+                allowances: StorageHashMap::new(),
+            }
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            *self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, who: AccountId) -> Balance {
+            self.balances.get(&who).copied().unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).copied().unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.inner_transfer(from, to, value)
+        }
+
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId, 
+            to: AccountId,
+            value: Balance
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance(from, caller);
+            if allowance < value {
+                return Err(Error::InsufficientAllowance);
+            }
+            self.allowances.insert((from, caller), allowance - value);
+            self.inner_transfer(from, to, value)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value);
+            self.env().emit_event( Approval {
+                owner, 
+                spender,
+                value
+            });
+            Ok(())
+        }
+
+        fn inner_transfer(
+            &mut self, 
+            from: AccountId, 
+            to: AccountId, 
+            value: Balance
+        ) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+
+            self.balances.insert(from, from_balance - value);
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, to_balance + value);
+            self.env().emit_event( Transfer {
+                from: Some(from),
+                to: Some(to),
+                value
+            });
+            Ok(())
+        }
+    }
+}
